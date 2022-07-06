dfcce608d25860944a02988b4c13df33e54b4247
tzubindev <71303907+tzubindev@users.noreply.github.com>
2022-07-06 17:04:30 +0800

    update

--- HighQuali/script.js
+++ HighQuali/script.js
@@ -1,26 +1,25 @@
 var Hi_Mid = 0;
 const HI_RIGHT = 170;
+let HI = document.querySelector("#w3");
 
 window.onscroll = function () {
     if (document.documentElement.scrollTop > 600) {
         document.getElementById("w3").style.position = "absolute";
-        document.getElementById("w3").style.top = "900px";
-        //document.getElementById("w3").style.right = (HI_MID * document.documentElement.scrollTop / 480) + "px";
+        document.getElementById("w3").style.top = "910px";
         document.getElementById("w3").style.right = HI_MID;
-        // if (document.documentElement.scrollTop < 800) {
-        //     document.getElementById("w3").style.right = (HI_RIGHT * document.documentElement.scrollTop / 480 * 2.5) + "px";
-        // }
+        HI.style.color = "#000000";
+
 
     } else {
         document.getElementById("w3").style.position = "fixed";
         document.getElementById("w3").style.top = "42%";
 
-        if (parsePXtoInt(getCssProperty("w3","right"))<=HI_RIGHT) {
+        if (parsePXtoInt(getCssProperty("w3", "right")) <= HI_RIGHT) {
             document.getElementById("w3").style.right = HI_RIGHT + "px";
         }
-        if(document.documentElement.scrollTop <= 600) {
-            document.getElementById("w3").style.right = (HI_RIGHT * 2.4 * document.documentElement.scrollTop/600 +HI_RIGHT) + "px";
-            if(document.documentElement.scrollTop === 600) Hi_Mid = getCssProperty("w3","right"); 
+        if (document.documentElement.scrollTop <= 600) {
+            document.getElementById("w3").style.right = (HI_RIGHT * 2.40 * document.documentElement.scrollTop / 600 + HI_RIGHT) + "px";
+            if (document.documentElement.scrollTop === 600) Hi_Mid = getCssProperty("w3", "right");
         }
     }
 }
@@ -31,6 +30,6 @@ function getCssProperty(elmId, property) {
 }
 
 function parsePXtoInt(value) {
-    value = value.replace("px","");
+    value = value.replace("px", "");
     return parseInt(value);
 }
